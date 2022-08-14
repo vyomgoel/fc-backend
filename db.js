67e068e57e7c025cb31368a50f63706f2d2fc1c0
Vyom <vyomthechamp@gmail.com>
2022-08-14 15:56:02 +0530

    update env variables and script

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const mongoURI =
-  "mongodb+srv://vyomgoel:vyomgoel12345@cluster0.daqjw.mongodb.net/inotebook";
+const mongoURI = process.env.MONGODB_URI;
+  //"mongodb+srv://vyomgoel:vyomgoel12345@cluster0.daqjw.mongodb.net/inotebook";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
