dde8cef3d6ee9bccb0bdff2d2247de96a40b77fa
Vyom <vyomthechamp@gmail.com>
2022-08-15 21:51:23 +0530

    change port to 5001

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoURI = process.env.MONGODB_URI;
-  //"mongodb+srv://vyomgoel:vyomgoel12345@cluster0.daqjw.mongodb.net/inotebook";
+//"mongodb+srv://vyomgoel:vyomgoel12345@cluster0.daqjw.mongodb.net/inotebook";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = 5001;
 var cors = require("cors");
 
 app.use(cors());
